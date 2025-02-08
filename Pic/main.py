415b8de96e24b0df53e18825912bcb93d8487a3b
jackngaijn <asdjack1012@gmail.com>
2025-02-09 01:51:11 +0800

    update paper

--- Pic/main.py
+++ Pic/main.py
@@ -2,22 +2,29 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Generate x values
-x = np.linspace(0, 8, 9)  # Values from 0 to 8 (inclusive)
+x = np.linspace(0, 15, 9)  # Values from 0 to 8 (inclusive)
 
 # Generate y values (exponential growth)
-y = 2 ** x  # Exponential function
+y1 = 0.5 ** x  # Exponential function
+y2 = 2 ** x  # Exponential function
 
 # Plot the graph
-plt.figure(figsize=(8, 6))
-plt.plot(x, y, marker='o', linestyle='-', color='b')
+fig = plt.figure()
+plt.figure(figsize=(8,4))
 
-# Add title and labels
-plt.title("Exponential Growth")
+plt.subplot(211)
+plt.plot(x, y1, marker='o', linestyle='-', color='b')
+plt.title("Exponential Grow")
 plt.xlabel("x")
 plt.ylabel("f(x)")
 
-# Add grid
-plt.grid()
+plt.subplot(212)
+plt.plot(x, y2, marker='o', linestyle='-', color='r')
+plt.xlabel("x")
+plt.ylabel("f(x)")
+
+# Save the figure as a PNG file
+plt.savefig("exponential_growth.png", dpi=300)  # Save with high resolution (300 DPI)
 
 # Show the plot
 plt.show()
